6c95cbc5de92c60020f64f2651066b326e602cb0
khanhsaker97 <khanhsaker1997@gmail.com>
2018-08-02 23:25:39 +0700

    edit cv

---
+++ index.js
@@ -0,0 +1,11 @@
+function sidebar(){
+	console.log("ahihi");
+	$('.sidebar-wrapper').addClass('keo')
+	return false;
+}
+
+function sidebarremove(){
+	console.log("ahihi");
+	$('.sidebar-wrapper').removeClass('keo')
+	return false;
+}
