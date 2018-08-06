dcf52781878220b18341d58e96ac0177a5c1036b
khanhsaker97 <khanhsaker1997@gmail.com>
2018-08-06 14:16:11 +0700

    edit web respensive

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function sidebar(){
-	console.log("ahihi");	
 	$('.sidebar-wrapper').addClass('keo')
 	return false;
 }
 
 function sidebarremove(){
-	console.log("ahihi");
 	$('.sidebar-wrapper').removeClass('keo')
 	return false;
 }
